b2f90fe5a9ca59b18fb28f42eaf37faff49fe9e9
ysy <youngshuyu1997@gmail.com>
2020-07-02 08:59:37 +0800

    update

---
+++ src/components/header/index.vue
@@ -0,0 +1,43 @@
+<template>
+        <el-row class="header" type="flex" justify="start" align="middle">
+            <el-col :span="5" class="name">文章后台管理</el-col>
+            <el-col :span="19">
+                <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                    <el-menu-item index="1">处理中心</el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">我的工作台</template>
+                        <el-menu-item index="2-1">选项1</el-menu-item>
+                        <el-menu-item index="2-2">选项2</el-menu-item>
+                        <el-menu-item index="2-3">选项3</el-menu-item>
+                        <el-submenu index="2-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="2-4-1">选项1</el-menu-item>
+                        <el-menu-item index="2-4-2">选项2</el-menu-item>
+                        <el-menu-item index="2-4-3">选项3</el-menu-item>
+                        </el-submenu>
+                    </el-submenu>
+                    <el-menu-item index="3" disabled>消息中心</el-menu-item>
+                    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+                </el-menu>
+            </el-col>
+        </el-row>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+    .name {
+        height: 61px;
+        line-height: 61px;
+        text-align: center;
+        font-size: 22px;
+        font-weight: 700;
+
+    }
+}
+</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>oa</div>
+  <div style="height:100vh;">
+    <el-container>
+      <el-header>
+        <v-header></v-header>
+      </el-header>
+    </el-container>
+    <el-container class="middle">
+      <el-aside>
+        <v-aside></v-aside>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import header from './header/index'
+import aside from './aside/index'
 export default {
-
+  name: 'main',
+  data () {
+    return {}
+  },
+  components: {
+    'v-header': header,
+    'v-aside': aside
+  }
 }
 </script>
 
 <style>
+@import '../../node_modules/normalize.css/normalize.css'
 
 </style>
